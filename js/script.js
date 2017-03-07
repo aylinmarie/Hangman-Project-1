8371a62077537937c1da6c4482fcd8782b1eb837
aylinmarie <aylinmariedesigns@gmail.com>
2017-03-07 14:25:38 -0500

    create framework for JS

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Load window properly
+window.onload = function(){
+
+// Global Variables
+var wordsAvailable = ["Denmark", "Djibouti", "Dominica", "Dominican Republic"];
+
+var letterBank = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+
+/// UI Updates ///
+const viewOnline = {
+  updateSecretWord: function(){
+    // Your Code Here
+  },
+  updateGuessNumbers: function(){
+    // Your Code Here
+  },
+  updateLetterBank: function(){
+  	// Your Code Here
+  }
+};
+
+
+/// Top-Level Application Code ///
+const gameController = {
+  handleClickLetter: function(){
+    // Your Code Here
+  },
+
+};
+
+// Click events
+
+
+};
