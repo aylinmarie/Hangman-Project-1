14edcd30792649e9ccf740300d7666dbcfefe2a6
aylinmarie <aylinmariedesigns@gmail.com>
2017-03-08 17:47:20 -0500

    CSS updates

--- script.js
+++ script.js
@@ -15,11 +15,13 @@ window.onload = function(){
   // var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
   // "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+
+
   /// Create Random Words///
   
 
     secretWord = wordsAvailable[Math.floor(Math.random() * wordsAvailable.length)];
-    alert("the secret word is " + secretWord);
+    // alert("the secret word is " + secretWord);
     for (var i = 0; i < secretWord.length; i++) {
       ourWord.push(secretWord[i]);
       placeholders.push("_ "); }
@@ -29,39 +31,22 @@ window.onload = function(){
  
     var clickLetter = $(".letter-bank .btn").click(function(){
 
-            $(this).css("backgroundColor", "white");
+            $(this).hide();
             var guess = this.innerHTML; 
             for (var k = 0; k < secretWord.length; k++) { 
               if (guess === secretWord[k]) {
-                // console.log(holderArray.push(ourWord[k]));
-                // holderArray[k] = guess;
-                placeholders[k] = guess;
+                placeholders[k] = guess; } }
 
-              // } else if (guess !== ourWord[k]) {
-              //   holderArray.push(" ");
-              //   counter.innerHTML = guessCount -= 1;
-              // } else { placeholders = holderArray; }
 
-             }
-           }
-             // holderArray.push(" ");  
+           //   } else if (guess !== secretWord[k]) { 
+           //      $( ".hangman" ).hide(); };
+           // }
 
               $(".secret-word").text(placeholders.join(" "));
-    
-             // console.log(ourWord);
+
     });
 
-
-
-  // showLetterBank();
-  // function showLetterBank() {
-  //    for (i = 0; i < alphabet.length; i++){
-  //         for(var j = 0; j < alphabet[i].length; j++) {
-  //         var keyboard = ("<input type='button' value='" + alphabet[i][j] + "'/>");
-  //         }
-  //         $("#buttons").append(keyboard); 
-  //       }
-  //  };
+  /// Create Name ///
 
 
   /// Hang Mouse ///
