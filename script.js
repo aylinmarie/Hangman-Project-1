b76bb2084a75fe661ae58ee88f25297cc22c4339
aylinmarie <aylinmariedesigns@gmail.com>
2017-03-07 21:14:27 -0500

    created new buttons for alphabet and click function

--- script.js
+++ script.js
@@ -1,34 +1,55 @@
 
 window.onload = function(){
   /// Global Variables ///
-  var wordsAvailable = ["Denmark", "Djibouti", "Dominica", "Dominican Republic"];
+  var wordsAvailable = ["Four", "See", "Eighteen", "Dominican Republic"];
+  var guessCount = 0;
+  var maxGuessCount = 6;
+  var placeholders = '';
+  // var letterBank = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
+  // "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-
-  var letterBank = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
-  "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  /// Guess function ///
+  guessNumbers();
+  function guessNumbers() {
+      if (guessCount >= maxGuessCount) {
+      alert("Sorry, you ran out of guesses. The answer was " + secretWord + ".");
+  }
+  };
 
 
   /// Letter Bank ///
 
-  showLetterBank();
-  function showLetterBank() {
-     for (var i = 0; i < letterBank.length; i++){
-         $(".letter-bank").text( $(".letter-bank").text() + letterBank[i] + "   ");  
-     }
-  };
+  // showLetterBank();
+  // function showLetterBank() {
+  //    for (var i = 0; i < letterBank.length; i++){
+  //        $(".letter-bank").text( $(".letter-bank").text() + letterBank[i] + "   ");  
+  //    }
+
+  // };
 
 
-/// Secret Word ///
+/// Create Random Words///
   
   var secretWord = wordsAvailable[Math.floor(Math.random() * wordsAvailable.length)];
+
   randomWord();
-  function randomWord() {
+    function randomWord() {
       $(".secret-word").text(secretWord);
+    };
+
+/// Create blank spaces ///
+
+  for (var j = 0; j < secretWord.length; j++) {
+      $("#secret").text((placeholders += '_ '));
+  }
+
+
+  // Click functions
+  selectLetter();
+  function selectLetter() {
+      $(".letter-bank .btn").click(function(){
+          $(this).css("backgroundColor", "white"); });
   };
 
-  wordLength();
-  function wordLength() {
 
-  };
-  // End
 };
